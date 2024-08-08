80cbbc90f23b69f93c5b33d26d219e6e2cfddfdc
Wei Han <xingheng907@hotmail.com>
2024-08-08 16:40:47 +0800

    Initial Commit

---
+++ EdgeWindow/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EdgeWindow
+//
+//  Created by WeiHan on 2024/8/8.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
